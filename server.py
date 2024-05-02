ef64dbc6a3487593aeac331a7843df3a10a8ce4c
Luis Noriega <lnoriegan3w@gmail.com>
2024-05-02 00:23:53 -0700

    adding bootstrap compiled css, js adding sign-in html

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('sign-in.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5006)
