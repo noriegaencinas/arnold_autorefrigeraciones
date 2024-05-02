c0712e7d7e1bab13b58715280d759b6599e26978
Luis Noriega <lnoriegan3w@gmail.com>
2024-05-02 13:45:12 -0700

    removing dist of css and js, instead replacing with CDN links to decrease the size of the app

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template('sign-in.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5006)
+    app.run(debug=True, port=5004)
