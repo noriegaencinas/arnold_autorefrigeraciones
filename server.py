2ad1614ddcf10dee0bfa0dc1d324e6c59673fd80
Luis Noriega <lnoriegan3w@gmail.com>
2024-05-02 17:50:45 -0700

    adding menu files

--- server.py
+++ server.py
@@ -11,6 +11,25 @@ def index():
 def menu():
     return render_template('menu.html')
 
+@app.route("/menu/inventario")
+def menu_inventario():
+    return render_template('menu_inventario.html')
+
+@app.route("/menu/automoviles")
+def menu_automoviles():
+    return render_template('menu_inventario.html')
+
+@app.route("/menu/empleados")
+def menu_empleados():
+    return render_template('menu_inventario.html')
+
+@app.route("/menu/financiero")
+def menu_financiero():
+    return render_template('menu_inventario.html')
+
+@app.route("/menu/facturas")
+def menu_facturas():
+    return render_template('menu_inventario.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5004)
