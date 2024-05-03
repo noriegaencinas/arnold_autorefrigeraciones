4c50e39f8037d76527bf6700a76ad5085bb2be62
Luis Noriega <lnoriegan3w@gmail.com>
2024-05-02 17:52:06 -0700

    fixing server.py to show correct menu files

--- server.py
+++ server.py
@@ -17,19 +17,19 @@ def menu_inventario():
 
 @app.route("/menu/automoviles")
 def menu_automoviles():
-    return render_template('menu_inventario.html')
+    return render_template('menu_automoviles.html')
 
 @app.route("/menu/empleados")
 def menu_empleados():
-    return render_template('menu_inventario.html')
+    return render_template('menu_empleados.html')
 
 @app.route("/menu/financiero")
 def menu_financiero():
-    return render_template('menu_inventario.html')
+    return render_template('menu_financiero.html')
 
 @app.route("/menu/facturas")
 def menu_facturas():
-    return render_template('menu_inventario.html')
+    return render_template('menu_facturas.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5004)
