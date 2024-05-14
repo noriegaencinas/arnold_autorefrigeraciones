ea715e47ae4b439c578f023878b4dd4fa244d16d
josea <joseale282003@gmail.com>
2024-05-13 18:41:11 -0700

    Fixin menu_empleados.js

--- controller/inventario_controller.py
+++ controller/inventario_controller.py
@@ -6,7 +6,10 @@ from model.inventario_model import *
 
 # Menu inventario
 
-@app.route("/menu/inventario")
+@app.route("/menu/automoviles")
+@app.route("/menu/automoviles/page")
+@app.route("/menu/automoviles/page/<int:page>")
 @login_required
-def menu_inventario():
-    return 'Inventario'
+def menu_automoviles(page=1):
+    rows = select_inventario_db(page)
+    return render_template('menu_inventario.html', data=rows)
--- model/inventario_model.py
+++ model/inventario_model.py
@@ -1,10 +1,43 @@
 import sqlite3
 from sqlite3 import Error
+from math import ceil
 
-def SelectInventario():
-    conn = sqlite3.connect("instance/arnold_autorefrigeraciones.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM inventario")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
+
+def select_inventario_db(page):
+    limit = 10
+    start = limit * (page - 1)
+    activo = 1
+    try:
+        with sqlite3.connect("instance/arnold_autorefrigeraciones.db") as conn:
+            c = conn.cursor()
+            c.execute("SELECT * FROM inventario WHERE activo = ? LIMIT ?, ?", (activo, start, limit))
+            myresult = c.fetchall()
+
+            # Convertir los datos a diccionario
+            insertObject = []
+            columnNames = [column[0] for column in c.description]
+            for record in myresult:
+                insertObject.append(dict(zip(columnNames, record)))                
+
+            # Obtener el número total de registros        
+            count = contar_activos_inventario()  
+            total_pages = ceil(count / limit)        
+        
+            return [insertObject, total_pages]
+    except sqlite3.Error as e:
+        return str(e)
+
+def contar_activos_inventario():
+    activo = 1
+    try:
+        conn = sqlite3.connect("instance/arnold_autorefrigeraciones.db")
+        c = conn.cursor()
+        # Get total number of records
+        c.execute("SELECT count(id_inventario) FROM inventario WHERE activo = ?", (activo, ))
+        count = c.fetchall()[0][0]        
+        return count
+    except Exception as e:
+        return str(e)
+    finally:
+        if conn:
+            conn.close()
