142d3cbbdd69219b4c1bfee23e1df873458b4c80
josea <joseale282003@gmail.com>
2024-05-13 18:55:00 -0700

    Fixing inventario_controller and menu_inventario

--- controller/inventario_controller.py
+++ controller/inventario_controller.py
@@ -6,10 +6,10 @@ from model.inventario_model import *
 
 # Menu inventario
 
-@app.route("/menu/automoviles")
-@app.route("/menu/automoviles/page")
-@app.route("/menu/automoviles/page/<int:page>")
+@app.route("/menu/inventario")
+@app.route("/menu/inventario/page")
+@app.route("/menu/inventario/page/<int:page>")
 @login_required
-def menu_automoviles(page=1):
+def menu_inventario(page=1):
     rows = select_inventario_db(page)
     return render_template('menu_inventario.html', data=rows)
