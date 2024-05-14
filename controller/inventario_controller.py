1f00cc2ee852724faf54fd9cf56478d5b2281867
josea <joseale282003@gmail.com>
2024-05-13 20:43:14 -0700

    Fixing menu_empleados.html

--- controller/inventario_controller.py
+++ controller/inventario_controller.py
@@ -1,8 +1,7 @@
 from main import app
-
 from controller.login_controller import *
-
 from model.inventario_model import *
+
 
 # Menu inventario
 
@@ -13,3 +12,48 @@ from model.inventario_model import *
 def menu_inventario(page=1):
     rows = select_inventario_db(page)
     return render_template('menu_inventario.html', data=rows)
+
+@app.route("/menu/inventario/agregar", methods=['GET', 'POST'])
+@login_required
+def crear_producto():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        cantidad = request.form['cantidad']
+        tipo= request.form['tipo']
+        descripcion = request.form['descripcion']
+        precio_compra = request.form['precio_compra']
+        precio_venta = request.form['precio_venta']
+        crear_producto_db( nombre, cantidad, precio_compra, tipo, descripcion, precio_venta)
+        return redirect(url_for('menu_inventario'))
+    return redirect(url_for('menu_inventario'))
+
+@app.route("/menu/inventario/modificar/<int:id_inventario>", methods=['GET', 'POST'])
+@login_required
+def actualizar_producto(id_inventario):
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        cantidad = request.form['cantidad']
+        tipo= request.form['tipo']
+        descripcion = request.form['descripcion']
+        precio_compra = request.form['precio_compra']
+        precio_venta = request.form['precio_venta']
+        actualizar_producto_db(id_inventario, nombre, cantidad, precio_compra, tipo, descripcion, precio_venta)
+        return redirect(url_for('menu_inventario'))
+    return redirect(url_for('menu_inventario'))
+
+@app.route("/menu/inventario/eliminar/<int:id_inventario>", methods=['GET', 'POST'])
+@login_required
+def eliminar_producto(id_inventario):
+    if request.method == 'POST':
+        eliminar_producto_db(id_inventario)   
+        return redirect(url_for('menu_inventario'))
+    return redirect(url_for('menu_inventario'))   
+
+@app.route("/menu/inventario/buscar", methods=['GET', 'POST'])
+@login_required
+def select_producto_by_filter():
+    if request.method == 'POST':
+        filtro = request.form['filtro']
+        rows = select_producto_by_filter_db(filtro)
+        return render_template('menu_inventario.html', data=rows)
+    return redirect(url_for('menu_inventario'))
--- model/inventario_model.py
+++ model/inventario_model.py
@@ -20,14 +20,14 @@ def select_inventario_db(page):
                 insertObject.append(dict(zip(columnNames, record)))                
 
             # Obtener el número total de registros        
-            count = contar_activos_inventario()  
+            count = contar_activos_inventario_db()  
             total_pages = ceil(count / limit)        
         
             return [insertObject, total_pages]
     except sqlite3.Error as e:
         return str(e)
 
-def contar_activos_inventario():
+def contar_activos_inventario_db():
     activo = 1
     try:
         conn = sqlite3.connect("instance/arnold_autorefrigeraciones.db")
@@ -41,3 +41,54 @@ def contar_activos_inventario():
     finally:
         if conn:
             conn.close()
+            
+def actualizar_producto_db(id_inventario, nombre, cantidad, precio_compra, tipo, descripcion, precio_venta):
+    try:
+        conn = sqlite3.connect("instance/arnold_autorefrigeraciones.db")
+        c = conn.cursor()
+        c.execute("UPDATE inventario SET Nombre = ?, Cantidad = ?, PrecioCompra = ?, Tipo = ?, Descripcion = ?, PrecioVenta = ? WHERE id_inventario = ?", (nombre, cantidad, precio_compra, tipo, descripcion, precio_venta, id_inventario))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print("Error:", e)
+        
+def eliminar_producto_db(id_inventario):
+    inactivo = 0
+    try:        
+        conn = sqlite3.connect("instance/arnold_autorefrigeraciones.db")
+        c = conn.cursor()
+        c.execute("UPDATE inventario SET Activo = ? WHERE id_inventario = ?", (inactivo, id_inventario))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print("Error:", e)
+
+def crear_producto_db(nombre, cantidad, precio_compra, tipo, descripcion, precio_venta):
+    try:
+        conn = sqlite3.connect("instance/arnold_autorefrigeraciones.db")
+        c = conn.cursor()
+        c.execute("INSERT INTO inventario (Nombre, Cantidad, PrecioCompra, Tipo, Descripcion, PrecioVenta) VALUES (?, ?, ?, ?, ?, ?)", (nombre, cantidad, precio_compra, tipo, descripcion, precio_venta))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print("Error:", e)
+        
+def select_producto_by_filter_db(filtro):
+    limit=10    
+    try:
+        conn = sqlite3.connect("instance/arnold_autorefrigeraciones.db")
+        conn.row_factory = sqlite3.Row  # This allows you to access rows as dictionaries
+        c = conn.cursor()
+        c.execute("SELECT * FROM inventario WHERE Nombre LIKE '%' || ? || '%'", (filtro,))
+        rows = [dict(row) for row in c.fetchall()]  # Convert rows to dictionaries
+        print(rows)
+        # Get total number of records
+        c.execute("SELECT count(id_inventario) FROM inventario WHERE Nombre LIKE '%' || ? || '%'", (filtro,))        
+        count = c.fetchall()[0][0]
+        total_pages = ceil(count / limit)
+        conn.close()
+        return [rows, total_pages]
+    except sqlite3.Error as e:
+        print("Error:", e)
+        return None
+   
