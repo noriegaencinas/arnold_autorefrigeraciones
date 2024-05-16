59c3ffd4b36a1eb28e6c7db67ebec58fbb773cd0
Luis Noriega <lnoriegan3w@gmail.com>
2024-05-16 00:22:52 -0700

    fixing agreagar cliente

--- model/cliente_model.py
+++ model/cliente_model.py
@@ -20,7 +20,7 @@ def agregar_cliente_db(nombre, telefono, correo):
     try:
         with sqlite3.connect("instance/arnold_autorefrigeraciones.db") as conn:
             c = conn.cursor()
-            c.execute("INSERT INTO clientes (nombre_cliente, telefono, correo) VALUES (?, ?, ?)", (nombre, telefono, correo))
+            c.execute("INSERT INTO clientes (nombre, telefono, correo) VALUES (?, ?, ?)", (nombre, telefono, correo))
             conn.commit()
     except sqlite3.Error as e:
         return [("error", str(e))]  # En caso de error, devolvemos una lista con una opción de error
