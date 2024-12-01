b3d80c57849978d9c8e363f0598908103dba54de
Luis Noriega <lnoriegan3w@gmail.com>
2024-12-01 01:32:33 -0700

    adding new module for invoicing clients

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Imports for Flask
 from flask import Flask
+from sec import ATK
 
 # Inicializando aplicación Flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '<key>'
-app.config['SQLALCHEMY_DATABASE_URI'] = '<db_path>'
+app.config['SECRET_KEY'] = 'miguelito'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///arnold_autorefrigeraciones.db'
 
 from controller.menu_principal_controller import *
 from controller.inventario_controller import *
@@ -12,8 +13,9 @@ from controller.automovil_controller import *
 from controller.financiero_controller import *
 from controller.empleado_controller import *
 from controller.reparaciones_controller import *
-from controller.factura_controller import *
+from controller.factura_empleado_controller import *
+from controller.factura_cliente_controller import *
 from controller.cliente_controller import *
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
