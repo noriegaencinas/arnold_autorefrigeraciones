39c9b2f9604d1b7bccb43678c1306591c53d2aa6
Luis Noriega <lnoriegan3w@gmail.com>
2024-12-02 03:19:48 -0700

    editing invocing clients

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ from controller.factura_cliente_controller import *
 from controller.cliente_controller import *
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True, host="0.0.0.0")
 # sdfgdfgd
